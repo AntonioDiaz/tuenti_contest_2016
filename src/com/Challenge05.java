98580ccd534843da8994942c0f908c43db0548c8
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-05-07 20:23:54 +0200

    Challenge 05 solution.

--- src/com/Challenge05.java
+++ src/com/Challenge05.java
@@ -26,7 +26,7 @@ public class Challenge05 {
 	private static final String HOST_IP = "52.49.91.111";
 	private static final int PORT_NUMBER = 9988;
 	private static final int INIT_WORD_SIZE = 4;
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		/* reading words */
 		List<LetterFrequency> lettersFrequencyList = null;
@@ -34,10 +34,10 @@ public class Challenge05 {
 			Scanner scanner = new Scanner(new File(INPUT_WORDS));
 			scanner.close();
 			Boolean finished = false;
-			Integer level = 1; 
 			while (!finished) {
+				Integer level = 1;
 				List<String> words = readWords(INIT_WORD_SIZE);
-				lettersFrequencyList = calculateFrecuency (words);
+				lettersFrequencyList = calculateFrecuency(words);
 				Socket echoSocket = new Socket(HOST_IP, PORT_NUMBER);
 				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
@@ -45,83 +45,88 @@ public class Challenge05 {
 				out.println(" ");
 				Boolean gameOver = false;
 				Set<LetterFrequency> lettersUsed = new HashSet<LetterFrequency>();
-				while (!gameOver) {
-					for (int i=0; i<10; i++) {
+				while (!gameOver && !finished) {
+					for (int i = 0; i < 10; i++) {
 						String line = in.readLine();
-						System.out.println(i + "-" + line);
-						if (line.contains("GAME OVER")) {
-							gameOver = true;
+						if (line != null) {
+							if (line.contains("GAME OVER")) {
+								gameOver = true;
+							} else if (line.contains("Your submit key is")) {
+								gameOver = true;
+								finished = true;
+								System.out.println(i + "-" + line);
+							} else {
+								System.out.println(i + "-" + line);
+							}
 						}
 					}
 					if (!gameOver) {
 						String gaps = in.readLine();
-						System.out.println(gaps);
+						System.out.println("gaps-->" + gaps);
 						if (gaps.contains("_")) {
-							if (!gameOver) {
+							if (!gameOver && !finished) {
 								gaps = gaps.replaceAll(" ", "");
 								gaps = gaps.replaceAll("_", "\\\\w");
 								List<String> filterList = filterList(words, gaps);
-								System.out.println("filterList -->" + filterList);
-								lettersFrequencyList = calculateFrecuency (filterList);
+								lettersFrequencyList = calculateFrecuency(filterList);
 								lettersFrequencyList.removeAll(lettersUsed);
 								LetterFrequency letterFrequency = lettersFrequencyList.get(0);
 								lettersUsed.add(letterFrequency);
-								System.out.println("Testing with...." + letterFrequency.getLetter());					
 								out.println(letterFrequency.getLetter().toString());
 							}
 						} else {
 							if (level.equals(1)) {
-								for (int i=0; i<3; i++) {				
-									String line = in.readLine();
-									System.out.println(i + " " + line);
+								for (int i = 0; i < 3; i++) {
+									in.readLine();
 								}
 							}
-							for (int i=0; i<2; i++) {				
+							for (int i = 0; i < 2; i++) {
 								String line = in.readLine();
-								System.out.println(i + " " + line);
+								System.out.println("second -->" + i + " " + line);
 							}
-								//								if (i==3) {
-								//									String string = (line.split(": "))[1];
-								//									System.out.println(level + "solution..." + string);
-								//								}
 							lettersUsed = new HashSet<LetterFrequency>();
 							words = readWords(INIT_WORD_SIZE + level);
 							out.println("a");
 							level++;
+							System.out.println("*****************************++Level" + level);
+							if (level.equals(14)) {
+								finished = true;
+								System.out.println(level);
+							}
 						}
 					}
 				}
 				echoSocket.close();
 				out.close();
-				in.close();					
+				in.close();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("challenge05 - finished");
 	}
-	
+
 	public static List<String> readWords(Integer wordSize) throws FileNotFoundException {
 		List<String> wordsList = new ArrayList<String>();
 		Scanner scanner = new Scanner(new File(INPUT_WORDS));
 		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
-			if (line.length()==wordSize) {
+			if (line.length() == wordSize) {
 				wordsList.add(line);
 			}
 		}
 		scanner.close();
 		return wordsList;
 	}
-	
-	
+
 	/**
 	 * Filter a list of words.
+	 * 
 	 * @param inputList
 	 * @param charArray
 	 * @return
 	 */
-	public static List<String> filterList(List<String>inputList, String patternStr) {
+	public static List<String> filterList(List<String> inputList, String patternStr) {
 		List<String> outputList = new ArrayList<String>();
 		Pattern pattern = Pattern.compile(patternStr);
 		for (String myWord : inputList) {
@@ -131,8 +136,8 @@ public class Challenge05 {
 		}
 		return outputList;
 	}
-	
-	private static List<LetterFrequency> calculateFrecuency (List<String> wordsList) {
+
+	private static List<LetterFrequency> calculateFrecuency(List<String> wordsList) {
 		Map<Character, Integer> mapFrequency = new HashMap<Character, Integer>();
 		for (String word : wordsList) {
 			char[] charArray = word.toCharArray();
@@ -144,7 +149,7 @@ public class Challenge05 {
 				}
 			}
 		}
-		System.out.println(mapFrequency);
+		// System.out.println(mapFrequency);
 		Set<Character> keySet = mapFrequency.keySet();
 		List<LetterFrequency> list = new ArrayList<LetterFrequency>();
 		for (Character character : keySet) {
@@ -154,7 +159,7 @@ public class Challenge05 {
 			list.add(letterFrequency);
 		}
 		Collections.sort(list, new LetterFrequencyComparator());
-		System.out.println("sort list--->" + list);
+		// System.out.println("sort list--->" + list);
 		return list;
 	}
 
@@ -162,41 +167,48 @@ public class Challenge05 {
 		@Override
 		public int compare(LetterFrequency o1, LetterFrequency o2) {
 			return o2.getRepetitions().compareTo(o1.repetitions);
-		}	
+		}
 	}
-	
+
 	static class LetterFrequency {
 		private Character letter;
 		private Integer repetitions;
-		
+
 		public Character getLetter() {
 			return letter;
 		}
+
 		public void setLetter(Character letter) {
 			this.letter = letter;
 		}
+
 		public Integer getRepetitions() {
 			return repetitions;
 		}
+
 		public void setRepetitions(Integer repetitions) {
 			this.repetitions = repetitions;
 		}
+
 		@Override
 		public int hashCode() {
 			return letter.hashCode();
 		}
-		
+
 		@Override
-		public boolean equals(Object other){
-		    if (other == null) return false;
-		    if (other == this) return true;
-		    if (!(other instanceof LetterFrequency))return false;
-		    return ((LetterFrequency)other).getLetter().equals(this.getLetter());
-		}		
-		
+		public boolean equals(Object other) {
+			if (other == null)
+				return false;
+			if (other == this)
+				return true;
+			if (!(other instanceof LetterFrequency))
+				return false;
+			return ((LetterFrequency) other).getLetter().equals(this.getLetter());
+		}
+
 		@Override
 		public String toString() {
 			return "LetterFrequency [letter=" + letter + ", repetitions=" + repetitions + "]";
 		}
-	 }	
+	}
 }
